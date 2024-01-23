17ae0ff6fa5ec525907f39cf8d5ed0330e844d92
rajhake710 <128530944+rajhake710@users.noreply.github.com>
2024-01-23 19:03:40 +0530

    Update Exception.java

--- Day1/Exception.java
+++ Day1/Exception.java
@@ -1,1 +1,40 @@
 
+
+class Program12
+{
+	public static void main(String args[])
+	{
+		
+		int num[] = new int[5];
+		
+		try
+		{
+			myFun();
+			System.out.println("below myFun()");
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Exception in main try catch :: "+e);
+		}
+		
+		System.out.println("main ends");
+		
+		
+	}
+	
+	static void myFun()
+	{
+		int res,a=10;
+		try
+		{
+			res=a/0;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("in myfun catch");
+		}
+			System.out.println("myfun complete");
+	}
+	
+}
