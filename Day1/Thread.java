6ad0743c6288f8f9694afe5bd34d5b51e7a1f0b9
rajhake710 <128530944+rajhake710@users.noreply.github.com>
2024-01-23 18:40:17 +0530

    Update Thread.java

--- Day1/Thread.java
+++ Day1/Thread.java
@@ -1,1 +1,30 @@
 
+class Mythread extends Thread
+{	
+  public boolean value;
+	public Mythread()
+	{		value=true;
+	}
+	
+	public void run()
+	{
+		int i=0;
+		while(value)
+		{
+			i++;
+			System.out.println(Thread.currentThread().getName()+" = "+i);
+		}
+	}
+}
+class Thread4
+{
+	public static void main(String args[]) throws InterruptedException
+	{
+		Mythread obj = new Mythread();
+		Thread t = new Thread(obj,"first");
+		t.start();
+		Thread.sleep(40);
+		obj.value=false;
+		System.out.println("main ends");
+	}
+}
